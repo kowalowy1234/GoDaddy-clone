2aaba9af5a919f161b1c3e7f3c871c9f37075c95
kowalowy1234 <kowalowy1234@gmail.com>
2023-10-29 14:51:48 +0100

    Hero section

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,17 +1,20 @@
 const headerEl = document.querySelector('.header');
 const mobileNavEl = document.querySelector('.mobile-nav');
 const mobileNavExitButton = document.querySelector('mobile-nav__exit-button')
+const bodyEl = document.querySelector('body');
 
 const headerClickHandler = (event) => {
     const clickedEl = event.target;
     
     if (clickedEl.className.includes('hamburger')) {
         mobileNavEl.classList.remove('not-visible');
+        bodyEl.classList.add('--disable-scroll');
         return;
     }
     
     if (clickedEl.className.includes('x-icon') || clickedEl.className === 'mobile-nav') {
         mobileNavEl.classList.add('not-visible');
+        bodyEl.classList.remove('--disable-scroll');
         return;
     }
 
